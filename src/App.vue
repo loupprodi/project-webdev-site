da9f2e2d5829da067d1a09ff874380b3dc78b9e5
Luiz Felipe V Prosdoskimi <82850974+loupprodi@users.noreply.github.com>
2023-03-30 21:09:36 -0300

    adding archives
    adding archives of a copy the webdev site

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <appHeader />
+    <router-view path="$router.key"/>
+    <!-- <homePage /> -->
+    <appFooter/>
+  </div>
+</template>
+
+<script>
+import './styles/global.css';
+import appHeader from './components/Header.vue';
+import appFooter from './components/Footer.vue';
+// import homePage from './pages/Home.vue';
+
+export default {
+  name: 'App',
+  components: { 
+    appHeader,
+    // homePage,
+    appFooter
+  }
+}
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,43 @@
+<template>
+    <main>
+        <div class="text-top">Videos novos todas as <b>quintas</b> às <b>20h</b></div>
+
+        <a href="https://youtube.com/wdevoficial" target="_blank">
+            <img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/subscribe.svg" alt="inscreva-se" id="subscribe">
+        </a>
+        <div class="text-bottom">youtube.com/wdevoficial</div>
+
+    </main>
+</template>
+<script>
+export default{
+    name: 'homePage'
+}
+</script>
+<style scoped>
+main{
+    background-color: var(--color-background-home) ;
+    align-items: center;
+    justify-content: center;
+}
+.text-top{
+    color: var(--color-text-light);
+    font-size: 22px;
+    margin-bottom: 20px;
+}
+
+#subscribe{
+    width: 340px;
+}
+.text-bottom{
+    color: var(--color-text-light);
+    font-size: 18px;
+    margin-top: 20px;
+}
+
+@media (min-width: 700px){
+    #subscribe{
+        width: 370px;   
+    }
+}
+</style>
